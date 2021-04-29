409c3cc74d9aa4f7f723d07a29afa72a6a27ef82
QiuYiliang <1162922438@qq.com>
2021-04-29 15:39:14 +0800

    初始化项目

---
+++ src/main/java/com/qingyuan/redtour/RedTourApplication.java
@@ -0,0 +1,13 @@
+package com.qingyuan.redtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedTourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qingyuan/redtour/RedTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingyuan.redtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedTourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
