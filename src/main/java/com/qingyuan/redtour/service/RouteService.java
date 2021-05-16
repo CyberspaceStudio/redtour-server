a185a385b803827684cdec28a8222047373e8806
laimuxi <1721261216@qq.com>
2021-05-16 16:58:23 +0800

    feat ： 完成routecontroller中的前四个方法，新增与route相关的mapper，dao和service

---
+++ src/main/java/com/qingyuan/redtour/service/RouteService.java
@@ -0,0 +1,51 @@
+package com.qingyuan.redtour.service;
+
+import com.qingyuan.redtour.pojo.BO.RouteBO;
+import com.qingyuan.redtour.pojo.Route;
+import com.qingyuan.redtour.utils.ResponseResult;
+
+import java.util.List;
+
+/**
+ * @Author lmx
+ * @Date 2021/5/15 22:34
+ * @Version 1.0
+ */
+public interface RouteService {
+    /**
+     * 根据分类查询路线
+     * @param category  1 代表红色中国；2 代表小康中国；3 代表复兴中国；4 代表游览本地
+     * @return 返回Route的route集合
+     */
+    ResponseResult<List<Route>> getRouteByCategory(Integer category);
+
+    /**
+     * 根据routeId获得分类中的路线详细
+     * @param routeId
+     * @return
+     */
+    ResponseResult<RouteBO> getRouteBoByRouteId(Integer routeId);
+
+    /**
+     * 将旅游路线加入用户计划
+     * @param userId
+     * @param routeId
+     * @return
+     */
+    ResponseResult<Void> addRouteToUserPlan(Integer userId, Integer routeId);
+
+    /**
+     * 将路线加入用户喜欢
+     * @param userId
+     * @param routeId
+     * @return
+     */
+    ResponseResult<Void> addRouteToUserStar(Integer userId, Integer routeId);
+
+    /**
+     * 添加路线
+     * @param routeBO
+     * @return
+     */
+    ResponseResult<Void> addRoute(RouteBO routeBO);
+}
--- src/main/java/com/qingyuan/redtour/utils/ResponseEnum.java
+++ src/main/java/com/qingyuan/redtour/utils/ResponseEnum.java
@@ -13,6 +13,15 @@ public enum ResponseEnum {
 
     // 用户错误 2001-2999
     USER_HAVE_EXIST(2001, "用户已存在"),
+    USER_NO_EXIST(2002,"用户不存在"),
+
+    // 路线错误 3001-3999
+    CATEGORY_NO_EXIST(3001,"分类不存在"),
+    ROUTE_NO_EXIST(3002,"路线不存在"),
+
+    // 计划错误 4001-4999
+    PLAN_HAS_EXIT(4001,"路线已在计划中"),
+    STAR_HAS_EXIT(4001,"路线已在收藏中"),
     ;
     /**
      * 状态码
